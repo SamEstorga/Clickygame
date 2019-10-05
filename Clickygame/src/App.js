305691af60fbafab8fa1e0768cb42b90ef1bc485
Sam Estorga <estorgajr@gmail.com>
2019-10-04 19:47:27 -0700

    created a monster

--- Clickygame/src/App.js
+++ Clickygame/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
-import Header from "./components/Header";
+import Card from "./Components/Card";
+import Wrapper from "./Components/Wrapper";
+import Header from "./Components/Header";
 import cards from "./cards.json";
 import "./App.css";
 
