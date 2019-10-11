4830d4aefb1677723c7511d6eaf67ba9577bbb91
Sam Estorga <estorgajr@gmail.com>
2019-10-11 10:43:28 -0700

    Updates

---
+++ precache-manifest.372f68d293ba2c3d26a137fd9d010967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1dfed6707427392ba8525762b3e138a",
+    "url": "/Clickygame/index.html"
+  },
+  {
+    "revision": "f5f487c9f6e80d1cb909",
+    "url": "/Clickygame/static/css/main.b035c481.chunk.css"
+  },
+  {
+    "revision": "9c9af24f920c821ced47",
+    "url": "/Clickygame/static/js/2.56af942b.chunk.js"
+  },
+  {
+    "revision": "f5f487c9f6e80d1cb909",
+    "url": "/Clickygame/static/js/main.04e81a11.chunk.js"
+  },
+  {
+    "revision": "2e4b528d1a3d9e4bea54",
+    "url": "/Clickygame/static/js/runtime-main.c711de8a.js"
+  }
+]);
